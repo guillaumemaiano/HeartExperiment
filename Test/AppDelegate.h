955688f9c1caa53283a4df1f4947231f553e1101
Guillaume Maiano <Guillaume.Maiano@locuz.nl>
2015-04-16 19:16:00 +0200

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by guillaume MAIANO on 16/04/15.
+//  Copyright (c) 2015 guillaume MAIANO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by guillaume MAIANO on 16/04/15.
+//  Copyright (c) 2015 guillaume MAIANO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
