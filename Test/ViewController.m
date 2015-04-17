4dabe9bac55c5dec198e3d6141e79f6b7428da32
Guillaume Maiano <Guillaume.Maiano@locuz.nl>
2015-04-17 16:53:31 +0200

    First going to simplify down

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -18,7 +18,7 @@
     CABasicAnimation * animation;
     int heartQuint;
     CGRect shapeLayerBounds;
-    //CALayer* heartLayer;
+    CALayer* heartLayer;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -40,13 +40,13 @@
     backgroundLayer.opaque = YES;
     backgroundLayer.hidden = NO;
     backgroundLayer.cornerRadius = 20.0;
-    backgroundLayer.frame = CGRectInset(self.view.layer.frame, 20, 20);
+    backgroundLayer.frame = CGRectInset(self.view.layer.frame, 60, 20);
     backgroundLayer.backgroundColor = [[UIColor colorWithRed:1.1 green:1 blue:0 alpha:1.0]CGColor];
     backgroundLayer.contents = (__bridge id) backgroundImg.CGImage;
     backgroundLayer.contentsScale = [[UIScreen mainScreen] scale];
     [self.view.layer addSublayer:backgroundLayer ];
     
-    CALayer* heartLayer =[CALayer layer];
+    heartLayer =[CALayer layer];
     heartLayer.contentsGravity = kCAGravityResizeAspect;
     heartLayer.shadowOffset = CGSizeMake(0, 3);
     heartLayer.shadowRadius = 5.0;
@@ -56,16 +56,6 @@
     heartLayer.contents = (__bridge id) heartImage.CGImage;
     heartLayer.contentsScale = [[UIScreen mainScreen] scale];
     heartLayer.masksToBounds = YES;
-    // prevent default animation
-    // (in our case, we could just use the base, given animation,
-    // but if you want to compelxify the animation, it might be better to have full control)
-    NSMutableDictionary *newActions = [[NSMutableDictionary alloc] initWithObjectsAndKeys:[NSNull null], @"onOrderIn",
-                                       [NSNull null], @"onOrderOut",
-                                       [NSNull null], @"sublayers",
-                                       [NSNull null], @"contents",
-                                       [NSNull null], @"bounds",
-                                       nil];
-    heartLayer.actions = newActions;
     [heartLayer setName:@"heartLayer"];
     [self.view.layer addSublayer:heartLayer];
     
@@ -115,7 +105,6 @@
     heartQuint%=5;
     float height = shapeLayerBounds.size.height * heartQuint/5.0;
     CGRect endBounds = CGRectMake(shapeLayerBounds.origin.x, shapeLayerBounds.origin.y, shapeLayerBounds.size.width, height);
-    CALayer * heartLayer = [self findLayerByName:@"heartLayer" inLayer:self.view.layer];
     [animation setFromValue:[NSValue valueWithCGRect:heartLayer.bounds]];
     [animation setToValue:[NSValue valueWithCGRect:endBounds]];
     [CATransaction begin];
@@ -126,48 +115,12 @@
     [CATransaction commit];
 }
 
-static inline double radians (double degrees) {
-    return degrees * M_PI/180;
-}
-
-UIImage* rotate(UIImage* src, UIImageOrientation orientation)
-{
-    UIGraphicsBeginImageContext(src.size);
-    
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    if (orientation == UIImageOrientationRight) {
-        CGContextRotateCTM (context, radians(90));
-    } else if (orientation == UIImageOrientationLeft) {
-        CGContextRotateCTM (context, radians(-90));
-    } else if (orientation == UIImageOrientationDown) {
-        // NOTHING
-    } else if (orientation == UIImageOrientationUp) {
-        CGContextRotateCTM (context, radians(90));
-    }
-    
-    [src drawAtPoint:CGPointMake(0, 0)];
-    
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return image;
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (CALayer *)findLayerByName:(NSString*) layerTagName inLayer:(CALayer*) layer{
-    
-    for (CALayer *sublayer in [layer sublayers]) {
-        
-        if ([[sublayer name] isEqualToString:layerTagName]) {
-            return sublayer;
-        }
-    }
-    
-    return nil;
-}
+
 
 @end
